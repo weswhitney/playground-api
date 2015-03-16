e53fc37bff5c410f669673ad1c62d8c447470536
weswhitney <wesleywhitney@gmail.com>
2015-03-16 09:52:10 -0600

    inital commit with rails-api and serializers

---
+++ config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.configuration.middleware.insert_before 0, "Rack::Cors" do
+  allow do
+    origins '*'
+    resource '*', :headers => :any, :methods => [:get, :post, :options]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+PlaygroundApp::Application.config.secret_token = '7a49d10b070700ee216d526dd603c60c9a6a0fecde62d87c063af30634027a69c148e505d049079592242cd80699474d2af23709377fcd758079115fa7ecafed'
